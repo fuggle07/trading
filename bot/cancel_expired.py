fd387f47fec0a59cac96fbc23b2bb3af0cb40320
Peter Fuggle <pfuggle@gmail.com>
2026-02-26 19:36:07 +1100

    linting

--- bot/cancel_expired.py
+++ bot/cancel_expired.py
@@ -4,9 +4,13 @@ from alpaca.trading.client import TradingClient
 from alpaca.trading.requests import GetOrdersRequest
 from alpaca.trading.enums import QueryOrderStatus
 
-client = TradingClient(os.getenv("ALPACA_API_KEY"), os.getenv("ALPACA_API_SECRET"), paper=True)
+client = TradingClient(
+    os.getenv("ALPACA_API_KEY"), os.getenv("ALPACA_API_SECRET"), paper=True
+)
 req = GetOrdersRequest(status=QueryOrderStatus.OPEN)
 orders = client.get_orders(req)
 for order in orders:
-    print(f"Cancelling {order.id} {order.symbol} {order.side} {order.qty} {order.time_in_force}")
+    print(
+        f"Cancelling {order.id} {order.symbol} {order.side} {order.qty} {order.time_in_force}"
+    )
     client.cancel_order_by_id(order.id)
